1b87dc555725ca0334c43dedfa3210bd85864c49
EleProjects <kyle541994@yahoo.com>
2023-12-07 13:06:27 -0600

    removed unnecessary variables

--- graphics.py
+++ graphics.py
@@ -3,8 +3,6 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
