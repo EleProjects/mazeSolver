573516ec29dc84d11c3ef53c56ee960c545baea0
EleProjects <kyle541994@yahoo.com>
2023-12-07 15:10:09 -0600

    functionality for drawing cells and development of cell class

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50, 50), Point(400,400))
-    win.draw_line(l, "black")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(25, 25, 50, 50)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(50, 50, 75, 75)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(75, 75, 100, 100)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(100, 100, 125, 125)
+
     win.wait_for_close()
 
 main()
