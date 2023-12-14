46a15918d43bc45a9dbbfc2536df1fabca99c97d
EleProjects <kyle541994@yahoo.com>
2023-12-14 16:02:48 -0600

    randomize maze

--- cell.py
+++ cell.py
@@ -1,22 +1,25 @@
 from graphics import Line, Point
 
+
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
-        self.has_top_wall = True
         self.has_right_wall = True
+        self.has_top_wall = True
         self.has_bottom_wall = True
         self.visited = False
         self._x1 = None
-        self._y1 = None
         self._x2 = None
+        self._y1 = None
         self._y2 = None
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
-        self._y1 = y1
         self._x2 = x2
+        self._y1 = y1
         self._y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
@@ -54,30 +57,30 @@ class Cell:
 
         fill_color = "red"
         if undo:
-            fill_color = "grey"
+            fill_color = "gray"
 
-        #moving left
+        # moving left
         if self._x1 > to_cell._x1:
             line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
             self._win.draw_line(line, fill_color)
-            line = Line(Point(x_mid, y_mid), Point(to_cell._x2, y_mid))
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_cell._x2, to_y_mid))
             self._win.draw_line(line, fill_color)
 
-        #moving right
+        # moving right
         elif self._x1 < to_cell._x1:
             line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
             self._win.draw_line(line, fill_color)
-            line = Line(Point(to_cell._x1, y_mid), Point(to_x_mid, to_y_mid))
+            line = Line(Point(to_cell._x1, to_y_mid), Point(to_x_mid, to_y_mid))
             self._win.draw_line(line, fill_color)
 
-        #moving up
+        # moving up
         elif self._y1 > to_cell._y1:
             line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
             self._win.draw_line(line, fill_color)
             line = Line(Point(to_x_mid, to_cell._y2), Point(to_x_mid, to_y_mid))
             self._win.draw_line(line, fill_color)
 
-        #moving down
+        # moving down
         elif self._y1 < to_cell._y1:
             line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
             self._win.draw_line(line, fill_color)
--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import unittest
+
 from maze import Maze
 
-class Test (unittest.TestCase):
+
+class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
-            num_cols
+            num_cols,
         )
         self.assertEqual(
             len(m1._cells[0]),
-            num_rows
+            num_rows,
         )
 
     def test_maze_break_entrance_and_exit(self):
@@ -28,5 +30,6 @@ class Test (unittest.TestCase):
             False,
         )
 
-        if __name__ == "__main__":
-            unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
